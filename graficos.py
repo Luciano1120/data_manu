4dc633b3d479da2e8a763c89b4d98aa2d04c405e
LucianoS <lucianoscorza@hotmail.com>
2025-02-02 21:01:46 -0300

    estructro streamlit con diferentes paginas dentro del main

--- graficos.py
+++ graficos.py
@@ -7,10 +7,10 @@ def grafico_barras(df, variable):
     Función para crear un gráfico de barras horizontales.
     Este tipo de Comentarios Doctrings se usan dentro de clases o funciones, si no se entienden como strings
     """
-    dfAg = df[variable].value_counts().reset_index()
-    dfAg.columns = [variable, "Cantidad"]  
+    dfAg = df[variable].value_counts().reset_index() #con reset_index lo convierto en dataframe
+    dfAg.columns = [variable, "Cantidad"]  #renombro las columnas, col 1 : con el nombre de la Variable y la 2 con la Cantidad
     total = df[variable].count()
-    dfAg["Porcentaje"] = (dfAg["Cantidad"] / total) * 100  # Calcula el porcentaje
+    dfAg["Porcentaje"] = (dfAg["Cantidad"] / total) * 100  # Calcula el porcentaje para cada fila
 
     # Configuración del gráfico
     plt.figure(figsize=(10, 6))
@@ -27,7 +27,41 @@ def grafico_barras(df, variable):
     for index, row in dfAg.iterrows():
         plt.text(row["Cantidad"] + 0.5,  # Posición a la derecha de la barra
                  index,                  # Posición en el eje y
-                 f'{row["Porcentaje"]:.1f}%', 
+                 f'{row["Cantidad"]} ({row["Porcentaje"]:.1f}%)',  # Concatenar cantidad y porcentaje, porcentaje se formate 
+                 va='center', ha='left', fontsize=10)
+
+    # Mostrar el gráfico en Streamlit
+    st.pyplot(plt)
+
+
+#alternativa sin valores sin Info
+def grafico_barras_sinInfo(df, variable):
+    """
+    Función para crear un gráfico de barras horizontales.
+    Pero quitando los valores sin Info 
+    """
+    dfFiltrado = df[df[variable] != "sin info"]  # Filtrar registros sin info
+    dfAg = dfFiltrado[variable].value_counts().reset_index() #con reset_index lo convierto en dataframe
+    dfAg.columns = [variable, "Cantidad"]  #renombro las columnas, col 1 : con el nombre de la Variable y la 2 con la Cantidad
+    total = dfFiltrado[variable].count()
+    dfAg["Porcentaje"] = (dfAg["Cantidad"] / total) * 100  # Calcula el porcentaje para cada fila
+
+    # Configuración del gráfico
+    plt.figure(figsize=(10, 6))
+    sns.barplot(data=dfAg, x="Cantidad", y=variable, palette="pastel")  # Gráfico horizontal
+
+    # Etiquetas y estilo
+    plt.xlabel("Cantidad", fontsize=12)
+    plt.ylabel(variable, fontsize=12)
+    plt.title(f"Distribución de {variable}", fontsize=14)
+    plt.grid(axis="x", linestyle="--", alpha=0.7)
+    plt.tight_layout()
+    
+    # Añadir etiquetas de porcentaje en las barras
+    for index, row in dfAg.iterrows():
+        plt.text(row["Cantidad"] + 0.5,  # Posición a la derecha de la barra
+                 index,                  # Posición en el eje y
+                 f'{row["Cantidad"]} ({row["Porcentaje"]:.1f}%)',  # Concatenar cantidad y porcentaje, porcentaje se formate 
                  va='center', ha='left', fontsize=10)
 
     # Mostrar el gráfico en Streamlit
